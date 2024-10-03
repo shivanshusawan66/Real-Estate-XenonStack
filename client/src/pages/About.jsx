3c1d81079b158913947d0e2ae4b719989399a96a
shivanshu66 <162125439+shivanshusawan66@users.noreply.github.com>
2024-10-03 19:47:51 +0530

    Add files via upload

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,15 @@
+// Desc: About page
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About XenonStack Real-Estate</h1>
+      <p className='mb-4 text-slate-700'>XenonStack Real Estate Solutions is at the forefront of transforming the way people buy, sell, and rent properties, leveraging data-driven insights and AI-powered tools. With a focus on the most sought-after locations, our platform simplifies the real estate process by offering seamless, end-to-end solutions for buyers, sellers, and renters.</p>
+      <p className='mb-4 text-slate-700'>
+      Our Mission: To empower clients in achieving their real estate objectives through intelligent data analytics, personalized services, and in-depth market expertise. Whether you're exploring property investments, selling a home, or looking for rental opportunities, XenonStack Real Estate is your trusted partner at every stage.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team of real estate professionals brings a wealth of industry knowledge and technical expertise, ensuring every client receives tailored support. We harness the power of advanced technologies to provide a transparent, efficient, and rewarding property transaction experience. At XenonStack, we make real estate smarter, faster, and more accessible for all.</p>
+    </div>
+  )
+}
